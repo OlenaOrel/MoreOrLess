8a34f584055c4ba231ac3d865359157fd6cc52e6
= <=>
2019-11-24 17:50:37 +0200

    Created getters and setters in Model, created random setter for secretNumber and tested it.

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -1,2 +1,35 @@
+/**
+ * Created by Olena Orel on 23.11.2019.
+ */
 public class Model {
+
+    private int secretNumber;
+
+    private int maxBarrier;
+    private int minBarrier;
+
+    public int getSecretNumber() {
+        return secretNumber;
+    }
+
+    public void setSecretNumber() {
+        secretNumber = (int) Math.ceil(Math.random() *
+                (maxBarrier - minBarrier - 1) + minBarrier);
+    }
+
+    public void setPrimaryBarrier(int minBarrier, int maxBarrier) {
+        this.minBarrier = minBarrier;
+        this.maxBarrier = maxBarrier;
+    }
+
+    public int getMaxBarrier() {
+        return maxBarrier;
+    }
+
+
+    public int getMinBarrier() {
+        return minBarrier;
+    }
+
+
 }
--- src/test/java/TestModel.java
+++ src/test/java/TestModel.java
@@ -1,2 +1,28 @@
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
 public class TestModel {
+
+    private static Model model;
+
+    @BeforeClass
+    public static void before() {
+        model = new Model();
+        model.setPrimaryBarrier(GlobalConstants.PRIMARY_MIN_BARRIER, GlobalConstants.PRIMARY_MAX_BARRIER);
+    }
+
+    // Test compiled successfully
+    @Ignore
+    @Test
+    public void testSetSecretNumber() {
+        for (int i = 0; i < 100000; i++) {
+            model.setSecretNumber();
+            Assert.assertTrue(model.getSecretNumber() > model.getMinBarrier());
+            Assert.assertTrue(model.getSecretNumber() < model.getMaxBarrier());
+        }
+    }
+
+
 }
